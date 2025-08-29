4b6da7d4f52a8fe2bf5c7113dda684cf9ca678cc
chloelee767 <echloelee@gmail.com>
2025-08-29 16:24:25 +0800

    revert thumb keys

--- keyboards/keebio/iris_ce/rev1/keymaps/chloelee767/keymap.c
+++ keyboards/keebio/iris_ce/rev1/keymaps/chloelee767/keymap.c
@@ -47,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
          /* Zxc Row (Right) */
          KC_N, LT(_NUM,KC_M), LT(_NUM,KC_COMM), LT(_NUM,KC_DOT), LT(_NUM,KC_SLSH), MO(_MULTIMEDIA),
          /* Bottom Row */
-         MO(_NUMNAV), MO(_SYMBOL), LT(_NUMNAV,KC_TAB),
+         LT(_NUMNAV,KC_DEL), LT(_SYMBOL,KC_BSPC), LT(_NUMNAV,KC_TAB),
          LT(_SYMBOL,KC_ENT), LT(_NUMNAV,KC_SPC), LT(_MULTIMEDIA,KC_BSPC)
          ),
 
